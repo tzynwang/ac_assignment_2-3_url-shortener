ac75993df4a98afcc84243fe65ca7fc8d6850d08
tzyn (windows) <tzyn.wang@gmail.com>
2021-07-14 11:53:52 +0800

    feat: generate short url

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const router = express.Router()
 
+// DB
+const URL = require('../../models/urls')
+
+// shorten URL function
+const { generateShortUrl } = require('../../tools/shortenUrl')
+
 router.get('/', (req, res) => {
   res.render('index', { input: true })
 })
 
-router.post('/', (req, res) => {
-  res.render('index', { result: true })
+router.post('/', async (req, res) => {
+  const { originUrl } = req.body
+  const shortUrl = await URL.find({ originUrl }).lean()
+
+  if (shortUrl.length) {
+    return res.render('index', {
+      result: true,
+      shortUrl: `${process.env.APP_URL}${shortUrl[0].shorten}`
+    })
+  }
+
+  let shorten = generateShortUrl()
+  let checkResult = await URL.find({ shorten }).lean()
+  while (checkResult.length) {
+    shorten = generateShortUrl()
+    checkResult = await URL.find({ shorten }).lean()
+  }
+
+  const newURL = new URL({
+    originUrl,
+    shorten
+  })
+  await newURL.save()
+  res.render('index', { result: true, shortUrl: `${process.env.APP_URL}${shorten}` })
 })
 
 module.exports = router
---
+++ tools/shortenUrl.js
@@ -0,0 +1,20 @@
+const container = []
+for (let i = 48; i < 58; i++) {
+  container.push(i)
+}
+for (let i = 65; i < 91; i++) {
+  container.push(i)
+}
+for (let i = 97; i < 123; i++) {
+  container.push(i)
+}
+
+function generateShortUrl () {
+  let shorten = ''
+  for (let i = 0; i < 5; i++) {
+    shorten += String.fromCharCode(container[Math.floor(Math.random() * container.length)])
+  }
+  return shorten
+}
+
+module.exports = { generateShortUrl }
